a385d74041fcb4a0509c81ff77e9fcf30ee10f27
Carlos Benítez <carlosalgabez.08@gmail.com>
2023-08-25 12:30:58 -0500

    longitud caracteres crear fondo

--- src/common/schemas/funds-schemas.ts
+++ src/common/schemas/funds-schemas.ts
@@ -7,16 +7,16 @@ export const fundsCrudValidator = yup.object({
     number: yup
         .string()
         .required("El campo es obligatorio")
-        .matches(/^[0-9]+$/, "Solo se permiten numeros")
-        .max(30, "Solo se permiten 30 caracteres"),
+        .test('len', 'Solo se permiten 30 caracteres', (val) => { if (val && val.toString().length > 30) return val.toString().length < 30; else return true}),
     denomination: yup
         .string()
         .required("El campo es obligatorio")
-        .max(100, "Solo se permiten 250 caracteres"),
+        .max(100, )
+        .test('len', "Solo se permiten 100 caracteres", (val) => { if (val && val.toString().length > 100) return val.toString().length < 100; else return true}),
     description: yup
         .string()
         .required("El campo es obligatorio")
-        .max(500, "Solo se permiten 500 caracteres"),
+        .test('len', "Solo se permiten 500 caracteres", (val) => { if (val && val.toString().length > 500) return val.toString().length < 500; else return true}),
     dateFrom: yup
         .date()
         .required("El campo es obligatorio")
