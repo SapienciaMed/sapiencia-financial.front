ad2233ae46a8f9702279896a267763983c961d31
JuanDGM <juan84_10@hotmail.com>
2023-10-13 17:21:02 -0500

    Se validaciones creacion pac

--- src/features/pac/createPac/page/pac-crud.page.tsx
+++ src/features/pac/createPac/page/pac-crud.page.tsx
@@ -1,35 +1,47 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
-import { useForm, Controller } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 import {
   ButtonComponent,
+  ButtonLoadingComponent,
   FormComponent,
   InputComponent,
   SelectComponent,
 } from "../../../../common/components/Form";
-import { useNavigate, useParams } from "react-router-dom";
 import { EDirection } from "../../../../common/constants/input.enum";
 import { usePacCrud } from "../hook/pac-crud.hook";
-import { BiPlusCircle } from "react-icons/bi";
 import { UploadComponent } from "../components/UploadComponent";
+import TableDataPropComponent from "../../../../common/components/tableDataProp.component";
 
 
 
 function PacCrud() {
 
-  const { errors, onSubmitPac, showModal, setMessage, register, isAllowSave, control, actualFullYear } = usePacCrud();
+  const { errors, onSubmitPac, showModal, setMessage, register, isAllowSave, control, actualFullYear, isVisibleTable, tableComponentRef, tableColumns, errorsPac, isLoading } = usePacCrud();
+
+  const btnUploadFileRef = useRef(null)
 
   /* let uploadFileRef = useRef<HTMLInputElement>(null) */
 
   const [visible, setVisible] = useState<boolean>(false);
   const [file, setfile] = useState<File>(null);
+  const [isVisibleErrors, setIsVisibleErrors] = useState(false)
+
 
   const getFile = (file: File) => {
     setfile(file)
 
     return file;
   }
+
+
+  const [dataTableSt, setDataTableSt] = useState<any>()
+
+  useEffect(() => {
+    setDataTableSt(errorsPac)
+  }, [errorsPac])
+
 
   return (
     <div className="crud-page">
@@ -38,7 +50,7 @@ function PacCrud() {
           Cargar archivo
         </p>
         <div className="card-user" >
-        
+
           <FormComponent action={onSubmitPac} id="form-pac" className="form-pac">
             <section className="grid-form-2-container-reverse grid-column-e-proj-operation mt-5px">
 
@@ -52,6 +64,7 @@ function PacCrud() {
                 direction={EDirection.column}
                 errors={errors}
               />
+
 
               <SelectComponent
                 idInput={`typeSource`}
@@ -67,6 +80,7 @@ function PacCrud() {
                 filter={true}
                 fieldArray={true}
                 errors={errors}
+                direction={EDirection.column}
               />
               <SelectComponent
                 idInput={`typePac`}
@@ -86,11 +100,12 @@ function PacCrud() {
                 filter={true}
                 fieldArray={true}
                 errors={errors}
+                direction={EDirection.column}
               />
 
-
+              <>{JSON.stringify(errors)}</>
             </section>
-            
+
             {/* <div className="title-button text-three large" style={{marginTop:'10px'}} onClick={()=>uploadFileRef.current?.click()}> Seleccionar archivo <BiPlusCircle /> </div>
             <input ref={uploadFileRef} type="file" style={{display:'none'}}/> */}
 
@@ -99,8 +114,19 @@ function PacCrud() {
               <br />
               <label className="upload-label" style={{ display: 'flex', alignItems: 'center' }} htmlFor="modal">Seleccionar archivo <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.00008 5.83331V11.1666" stroke="#533893" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M10.6666 8.50002H5.33325" stroke="#533893" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M8 14.5V14.5C4.686 14.5 2 11.814 2 8.5V8.5C2 5.186 4.686 2.5 8 2.5V2.5C11.314 2.5 14 5.186 14 8.5V8.5C14 11.814 11.314 14.5 8 14.5Z" stroke="#533893" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></label>
               {file != undefined ? (<label className='text-red-500'>{file.name}</label>) : (<></>)}
-
               <Button label="Show" type="button" style={{ display: 'none' }} name='modal' id='modal' onClick={() => setVisible(true)} />
+
+              {
+                errorsPac?.length > 0 && (
+                  <ButtonComponent
+                    className="button-main huge hover-three"
+                    value="Validación"
+                    type="button"
+                    action={() => setIsVisibleErrors(!isVisibleErrors)}
+                  />
+                )
+              }
+
               <Dialog
                 header="Si tienes más de un documento, se deben unir en un solo archivo para ser cargados"
                 className='text-center div-modal movil'
@@ -126,18 +152,66 @@ function PacCrud() {
                 <Button className='mt-8' type="button" style={{ backgroundColor: '533893' }} onClick={() => setVisible(false)} label="Cancelar" rounded />
               </Dialog>
             </div>
+            <input type="submit" style={{ display: 'none' }} ref={btnUploadFileRef} />
+          </FormComponent>
+        </div>
+        <br />
 
-            
-            <ButtonComponent
+        {
+          isVisibleErrors && (
+            <div
+              className={
+                !isVisibleTable ? "card-user isVisible" : "card-user isNotVisible"
+              }
+            >
+
+              <TableDataPropComponent
+                ref={tableComponentRef}
+                dataTable={dataTableSt}
+                columns={tableColumns}
+                isShowModal={false}
+                titleMessageModalNoResult={"No se encontraron registros"}
+                secondaryTitle="Validaciones"
+              />
+
+            </div>
+          )
+        }
+      </div>
+
+      <div className="container-button-bot">
+        <div className="buttons-bot" >
+          {/* <span
+            className="bold text-center button"
+          onClick={() => {
+            confirmClose(action === "new" ? onCancelNew : onCancelEdit, action);
+          }}
+          >
+            Cancelar
+          </span> */}
+          {/* <ButtonComponent
               className="button-main huge hover-three"
               value="Guardar"
-              form="form-pac"
               type="submit"
-              disabled={!isAllowSave}
-            />
-          </FormComponent>
-
+              form="funds-form"
+              disabled={true}
+            /> */}
+          <ButtonLoadingComponent
+            className="button-main huge hover-three"
+            value="Guardar"
+            form="form-pac"
+            type="button"
+            action={() => btnUploadFileRef.current.click()}
+            disabled={!isAllowSave}
+            isLoading={isLoading}
+          />
         </div>
+
+
+
+
+
+
       </div>
     </div>
   );
